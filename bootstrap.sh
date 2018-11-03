f67b4003e1d8e25f7a171df8aa314e3d6a58d568
Niek Vlessert <niekvlessert@gmail.com>
2018-11-03 19:30:43 +0100

    Fixed CentOS firewall thing...
    Why is iptables-save available if it doesn't work?
    Discovered I need iptables-save..

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,13 @@ NO_YUM=$?
 RESULT=$(which apt-get > /dev/null 2>&1)
 NO_APT=$?
 
+RHEL7=0
+
+RESULT=$(cat /etc/*-release | grep -e "CentOS\|RedHat" | grep 7)
+if (( $? == 0 )); then
+	RHEL7=1
+fi
+
 if (( $NO_YUM == 1 && $NO_APT == 1));
 then
 	echo "No Yum or Apt detected, your system is unsupported... please install the required packages manually."
@@ -62,13 +69,21 @@ then
 	chkconfig postgresql on
 fi
 
-echo "Adding a line to the IPTables to allow syslog information on UDP port 514..."
-iptables -D INPUT -p udp --dport 514 -j ACCEPT > /dev/null 2>&1
-iptables -I INPUT -p udp --dport 514 -j ACCEPT
-echo "Adding a line to the IPTables to allow http on TCP port 80..."
-iptables -D INPUT -p tcp --dport 80 -j ACCEPT > /dev/null 2>&1
-iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-iptables-save > /dev/null 2>&1
+if (( $RHEL7 == 1 ));
+then
+	firewall-cmd --permanent --add-service=syslog > /dev/null 2>&1
+	firewall-cmd --reload > /dev/null 2>&1
+	echo "RHEL7/CentOS 7 detected... adding firewall rule using firewall-cmd"
+else
+	echo "Adding a line to the IPTables to allow syslog information on UDP port 514..."
+	iptables -D INPUT -p udp --dport 514 -j ACCEPT > /dev/null 2>&1
+	iptables -I INPUT -p udp --dport 514 -j ACCEPT
+	echo "Adding a line to the IPTables to allow http on TCP port 80..."
+	iptables -D INPUT -p tcp --dport 80 -j ACCEPT > /dev/null 2>&1
+	iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+	iptables-save > /dev/null 2>&1
+fi
+
 echo "Disabling SELinux..."
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
